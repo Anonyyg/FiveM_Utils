ccc678dc38fd94ccb4aaee2b2b24ed53dad5312e
Anonyyg <68130220+Anonyyg@users.noreply.github.com>
2020-07-11 00:56:07 +0100

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+-- https://docs.fivem.net/docs/scripting-reference/resource-manifest/resource-manifest/
+
+description 'fivem-utility-resources'
+
+version '1.0.0'
+
+server_scripts {
+
+}
+
+client_scripts {
+	'client/prevent_pistolwhip.lua'
+}
